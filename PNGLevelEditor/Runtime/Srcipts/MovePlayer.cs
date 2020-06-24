af2d41ced4e2ed193e84c36a643f7ddcd52cdb28
ward <ward@petrus-games.eu>
2020-06-24 22:24:52 +0200

    materials added

---
+++ PNGLevelEditor/Runtime/Srcipts/MovePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public Camera  _camera;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0,-0.5f,0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, 0.05f));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -0.05f));
+        }
+        _camera.transform.position = new Vector3(transform.position.x , transform.position.y + 15, transform.position.z - 15);
+    }
+}
