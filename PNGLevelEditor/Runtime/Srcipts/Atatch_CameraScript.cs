8d97630f73ae294d6783563c37a9cefe0038db0e
ward <ward@petrus-games.eu>
2020-06-24 18:04:00 +0200

    n

---
+++ PNGLevelEditor/Runtime/Srcipts/Atatch_CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atatch_CameraScript : MonoBehaviour
+{
+    public MovePlayer movePlayerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(movePlayerScript._camera == null)
+        {
+            movePlayerScript._camera = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
